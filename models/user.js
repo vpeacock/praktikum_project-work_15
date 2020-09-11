74299f9d0c3bc316c9d2bf9dcd1c767f84b32437
[Valentina] <[mariaiscus1@gmail.com]>
2020-09-11 11:04:19 +0600

    Исправить код для централизованной обработки ошибок

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
+const UnauthorizedError = require('../errors/unauthorized-err');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -37,12 +38,12 @@ userSchema.statics.findUserByCredentials = function findUserByCredentials(email,
     .select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
+        throw new UnauthorizedError('\u{261E} \u{26A0} Неправильные почта или пароль');
       }
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new Error('Неправильные почта или пароль'));
+            throw new UnauthorizedError('\u{261E} \u{26A0} Неправильные почта или пароль');
           }
 
           return user;
